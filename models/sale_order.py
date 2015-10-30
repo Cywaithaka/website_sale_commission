77ac6b3f731149367d61af943bebf6341736ee73
Cyrus Waithaka <cygawa@gmail.com>
2015-10-30 03:05:16 +0300

    Working copy on website sales. Cleaned up

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,19 +3,14 @@
 #
 #
 ##############################################################################
-
-#from openerp import models, fields, api
-from openerp import SUPERUSER_ID,models,api
-from openerp.osv import osv, fields
-class SaleOrder(osv.Model):
+from openerp import SUPERUSER_ID,models,api, fields
+class SaleOrder(models.Model):
     _inherit = "sale.order"
-
     def _website_product_id_change(self, cr, uid, ids, order_id, product_id, qty=0, line_id=None, context=None):
         res = super(SaleOrder,self)._website_product_id_change(cr, uid, ids, order_id, product_id, qty=qty, line_id=line_id, context=None)
-        #if not vals.get('agents', False):
-        if not 'agents' in res:
+        if qty==1:#if 'agents' not in res: #Only do this for the first item
             line = self.pool.get('sale.order.line').browse(cr, SUPERUSER_ID, line_id, context=context)
-            res['agents'] = line.set_agents(cr, uid, line.order_partner_id.id,context=context)
+            res.update({'agents': line.set_agents(cr, uid, line.order_partner_id.id,context=context)})
         return res
 
 class SaleOrderLine(models.Model):
